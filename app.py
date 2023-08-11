6d16c8901f80ae42656683dc89ceb550dfaaea00
Miguel Angel Valdez Navarrete <55571730+Mixcoatl120@users.noreply.github.com>
2023-08-11 12:46:58 -0600

    Hola(¬¬)

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from flask_boostrap import Bostrap
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return '<h1>hola mundo</h1> <button type="button" class="btn btn-primary btn-sm">Small button</button>'
+    return '<h1>hola mundo</h1> <button type="button" class="btn btn-primary btn-sm">Consultas</button>'
 
 @app.route('/consultas')
 def consulta():
