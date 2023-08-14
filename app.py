f304458f6411022fdda569ce92071e97d1171fb4
Miguel Angel Valdez Navarrete <55571730+Mixcoatl120@users.noreply.github.com>
2023-08-14 10:30:27 -0600

    Hola(~_~)

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ bootstrap = Bootstrap(app)
 @app.route('/')
 def home():
     return render_template('home.html')
-#'<h1>hola mundo</h1> <button type="button" class="btn btn-primary btn-sm">Consultas</button>'
+#'<h1>hola mundo</h1> '
 
 if __name__ == '__main__':
     app.run(debug = True)
