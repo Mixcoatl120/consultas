269f17481fd9c71bcb98ddf10a6c00456aa387d7
Miguel Angel Valdez Navarrete <55571730+Mixcoatl120@users.noreply.github.com>
2023-08-21 13:04:31 -0600

    Hola

--- app.py
+++ app.py
@@ -24,24 +24,20 @@ def home():
     if request.method == 'POST':
         f1 = request.form['f1']
         f2 = request.form['f2']
-        m = request.form['materia']
+        #m = request.form['materia']
         print(str(f1))
         print(str(f2))
-        print(str(m))
+        #print(str(m))
     return render_template('home.html')
 
 @app.route('/users')
 def users():
     conn = conexion()
     cursor = conn.cursor()
-    if request.method == 'GET':
-        f1 = request.form['f1']
-        f2 = request.form['f2']
-
     #consulta con select de prueba
-        cursor.execute("SELECT seguimiento.fsolicitud, cat_tipo_ingreso.tipo_ingreso, cat_materia.materia, cat_tramites.cofemer,seguimiento.rnomrazonsolcial, dir_gral.siglas,cat_estatus.estatus   FROM seguimiento LEFT JOIN cat_tipo_ingreso ON seguimiento.tipo_ingreso = cat_tipo_ingreso.id LEFT JOIN cat_tipo_asunto ON seguimiento.tipo_asunto = cat_tipo_asunto.id LEFT JOIN cat_descripcion ON seguimiento.descripcion = cat_descripcion.id LEFT JOIN cat_materia ON seguimiento.materia = cat_materia.id LEFT JOIN cat_tramites ON seguimiento.tramite = cat_tramites.idtram LEFT JOIN cat_tipoinstalacion  ON seguimiento.tipoinstalacion = cat_tipoinstalacion.id LEFT JOIN cat_actividad ON seguimiento.cve_actividad = cat_actividad.id LEFT JOIN cat_personal AS evaluador ON seguimiento.nevaluador = evaluador.idpers LEFT JOIN cat_personal AS aar ON seguimiento.persona_ingresa = aar.idpers LEFT JOIN dir_gral ON seguimiento.dirgralfirma = dir_gral.id LEFT JOIN cat_sitact ON seguimiento.situacionactualtram = cat_sitact.id LEFT JOIN cat_sentido_resolucion ON seguimiento.sentido_resolucion = cat_sentido_resolucion.id LEFT JOIN cat_estatus ON seguimiento.estatus_tramite = cat_estatus.id WHERE seguimiento.fsolicitud >= ""'"+f1+"'"" seguimiento <= ""'"+f2+"'"" ")
-        users = cursor.fetchall()
-        conn.close()
+    cursor.execute("SELECT seguimiento.fsolicitud, cat_tipo_ingreso.tipo_ingreso, cat_materia.materia, cat_tramites.cofemer,seguimiento.rnomrazonsolcial, dir_gral.siglas,cat_estatus.estatus   FROM seguimiento LEFT JOIN cat_tipo_ingreso ON seguimiento.tipo_ingreso = cat_tipo_ingreso.id LEFT JOIN cat_tipo_asunto ON seguimiento.tipo_asunto = cat_tipo_asunto.id LEFT JOIN cat_descripcion ON seguimiento.descripcion = cat_descripcion.id LEFT JOIN cat_materia ON seguimiento.materia = cat_materia.id LEFT JOIN cat_tramites ON seguimiento.tramite = cat_tramites.idtram LEFT JOIN cat_tipoinstalacion  ON seguimiento.tipoinstalacion = cat_tipoinstalacion.id LEFT JOIN cat_actividad ON seguimiento.cve_actividad = cat_actividad.id LEFT JOIN cat_personal AS evaluador ON seguimiento.nevaluador = evaluador.idpers LEFT JOIN cat_personal AS aar ON seguimiento.persona_ingresa = aar.idpers LEFT JOIN dir_gral ON seguimiento.dirgralfirma = dir_gral.id LEFT JOIN cat_sitact ON seguimiento.situacionactualtram = cat_sitact.id LEFT JOIN cat_sentido_resolucion ON seguimiento.sentido_resolucion = cat_sentido_resolucion.id LEFT JOIN cat_estatus ON seguimiento.estatus_tramite = cat_estatus.id WHERE seguimiento.fsolicitud >= ""'"+f1+"'"" seguimiento <= ""'"+f2+"'"" ")
+    users = cursor.fetchall()
+    conn.close()
     return render_template('users.html', users=users)
     #return render_template('users.html')
 
